73e925ecd254e196670e5cb6340888b6d0cbc487
Makan <makan@makan.bz>
2022-07-06 19:13:55 -0400

    Porting over to Next.js! (#9)
    * chore: create Next.js project (#6)
    
    * chore: next.js base setup
    
    * chore: dev setup
    
    * chore: Port over Jekyll website to Next.js (#8)
    
    * chore: dependabot and _document creation
    
    * chore: remove next.js defaults and begin port
    
    * chore: navbar and draft homepage
    
    * chore: completed header and added experience section (draft)
    
    * chore: fixed image sizing and added side projects
    
    * chore: some minor css changes and projects section completed
    
    * chore: add legacy pages
    
    * feat: new homepage

---
+++ components/shared/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        Designed and developed by Makan &#169; 2022
+      </div>
+    </footer>
+  );
+};
