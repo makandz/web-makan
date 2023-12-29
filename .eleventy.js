1ceb0aef9ec6c2c0a0cafe743b113e014e2f8102
MastaCoder <makan@makan.bz>
2023-12-29 17:26:06 -0500

    Updated builds and added legacy static pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 const PostCSSPlugin = require('eleventy-plugin-postcss');
+const directoryOutputPlugin = require('@11ty/eleventy-plugin-directory-output');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(PostCSSPlugin);
   eleventyConfig.addPassthroughCopy('src/assets/img');
+  eleventyConfig.setQuietMode(true);
+  eleventyConfig.addPlugin(directoryOutputPlugin);
 
   return {
     dir: {
